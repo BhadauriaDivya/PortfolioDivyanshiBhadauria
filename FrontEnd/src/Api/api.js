cd431bebb355225d51fc137022994b3b075486da
Divyanshi Bhadauria <bdivyanshi11.11@gmail.com>
2026-02-21 15:09:50 +0530

    Update api.js

--- FrontEnd/src/Api/api.js
+++ FrontEnd/src/Api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { BACKEND_URL } from "../config";
+import { VITE_BACKEND_URL } from "../config";
 
 const API = axios.create({
-  baseURL: BACKEND_URL, // ✅ must use REACT_APP_ prefix
+  baseURL: VITE_BACKEND_URL, // ✅ must use REACT_APP_ prefix
   headers: {
     "Content-Type": "application/json",
   },
