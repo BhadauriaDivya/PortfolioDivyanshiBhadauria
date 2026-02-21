38f0dd25398058de9c7194ae102e0d3a330d111b
Divyanshi Bhadauria <bdivyanshi11.11@gmail.com>
2026-02-21 15:35:39 +0530

    Update config.js

--- FrontEnd/src/config.js
+++ FrontEnd/src/config.js
@@ -1,2 +1,2 @@
 // src/config.js
-export const VITE_BACKEND_URL = "https://divyanshi-bhadauria-portfolio-backend.onrender.com";
+export const VITE_BACKEND_URL = "https://portfoliodivyanshibhadauria-backend.onrender.com/";
